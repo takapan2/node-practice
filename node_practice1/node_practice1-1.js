f3d875d833f3b658f30265eb3015f8ed1d068305
takapan2 <boysbeambitious_1161@yahoo.co.jp>
2021-01-05 16:28:10 +0900

    node_practice1-1

---
+++ node_practice1/node_practice1-1.js
@@ -0,0 +1,20 @@
+const asyncFunc = () => {
+    // return new Promise(r => {
+    //     setTimeout(() => {
+    //         console.log('Running!');
+    //         r();
+    //     }, 1000);
+    // });
+    return (r) => {
+        setTimeout(() => {
+            console.log("Running!");
+            r();
+        }, 1000);
+    };
+};
+
+(async() => {
+    console.log("Calling async func!");
+    asyncFunc();
+    console.log("async func called");
+})();
